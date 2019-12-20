e29a346112827f3833987a1c651c360af83241be
akhildevops19 <akhil.devops19@gmail.com>
2019-12-19 22:59:36 -0500

    terraform aws main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+ Volume in drive C is OS
+ Volume Serial Number is 5C98-E484
+
+ Directory of C:\Users\akkia\Git-Rep
+
+12/19/2019  10:54 PM    <DIR>          .
+12/19/2019  10:54 PM    <DIR>          ..
+12/19/2019  10:54 PM                 0 main.tf
+               1 File(s)              0 bytes
+               2 Dir(s)  102,095,998,976 bytes free
